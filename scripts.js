e298809083e2aced74c7093e9f8690e260c64f64
morgs <mwhapeles@ewu.edu>
2025-06-09 17:11:54 -0700

    created boiler and added assets

---
+++ scripts.js
@@ -0,0 +1,11 @@
+const card = document.querySelector('.card');
+
+
+card.addEventListener('click', () => {
+    card.classList.toggle('flipped');
+  
+// test(); is connected to the confetti function    
+    test();
+});
+
+console.log('hello world');
